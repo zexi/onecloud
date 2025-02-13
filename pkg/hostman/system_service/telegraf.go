0a59d5db7da35c11042cbcb6eb56032421234d0c
Zexi Li <zexi.li@icloud.com>
2025-02-13 11:45:15 +0800

    fix(lbagent): telegraf configuration panic

--- pkg/hostman/system_service/telegraf.go
+++ pkg/hostman/system_service/telegraf.go
@@ -169,7 +169,9 @@ func (s *STelegraf) GetConfig(kwargs map[string]interface{}) string {
 	ignorePathSegments := []string{
 		"/run/k3s/containerd/",
 	}
-	ignorePathSegments = append(ignorePathSegments, kwargs["server_path"].(string))
+	if sp, ok := kwargs["server_path"]; ok {
+		ignorePathSegments = append(ignorePathSegments, sp.(string))
+	}
 	for i := range ignorePathSegments {
 		ignorePathSegments[i] = fmt.Sprintf("%q", ignorePathSegments[i])
 	}
